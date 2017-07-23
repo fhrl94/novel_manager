a0519d0528bcdd3300610ecb1c8032938982af26
fhrl94 <445695121@qq.com>
2017-07-23 22:48:56 +0800

    重写了项目
    book主要是
    打开网页，自动解码
    获取小说名称及目录url写到sqlite数据库中
    在目录url中查找第一条章节的可打开链接，如果数据库中不存在，则添加。
    将上面的url以及其他数据，存到BookList中
    根据BookList chaptercontent为空的，且status为True的行，打开章节链接，进行更新
    查找status为True发送邮件
    
    主函数循环

--- book.py
+++ book.py
@@ -1,67 +1,168 @@
+import smtplib
+from datetime import datetime
+import time
+import re
 from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
-import smtplib
 
-import time
-import urllib.request
-
-import re
+import  requests
 from bs4 import BeautifulSoup as bs
+from requests import ConnectTimeout
+# from requests.exceptions import ConnectTimeout
+from sqlalchemy.orm.exc import NoResultFound
+
+from novel_manager.stone import stoneobject, BookName, BookList
+
+# 全局数据库链接
+global stone
+stone=stoneobject()
+# 全局list TAG标签
+mapping = []
+file = open('mapping.txt', 'rb')
+for f in file.readlines():
+    # print(f.decode('utf-8').replace('\r\n', '', -1))
+    mapping.append(f.decode('utf-8').replace('\r\n', '', -1))
+# print(mapping)
+#
+
+global from_addr,password,to_addr
+from_addr=input("请输入发件人邮箱")
+password=input("请输入发件人邮箱密码")
+to_addr=input("请输入收件人邮箱")
 
 
-# 打开网页，默认是sodu搜索页面，参数为空，返回为utf-8的字符型
-from sqlalchemy.orm.exc import  NoResultFound
+# 打开网页,基于charset，如果设置为设置值，默认为utf-8,返回为解码后的源码
+def OpenUrl(url,timeout=10):
+    r=requests.get(url=url,timeout=timeout)
+    charset = re.compile(r'charset=["]?(.*?)"')
+    encoding = re.findall(charset, r.text)
+    # print(type(encoding))
+    r.encoding='utf-8'
+    if encoding :
+        r.encoding = encoding[0]
+    # print(r.text)
+    return r.text
 
-from study.stone import save, query
+# 使用bs解析str,返回结构化文档
+def ParsingString(string):
+    return bs(string,'html.parser')
+
+# 获取小说名称
+def __GetBookName():
+
+    stone=stoneobject()
+    f=open('book.txt','ab+')
+    f.seek(0)
+    if len(f.read())==0:
+        str=input("请输入小说名称")
+        f.write(str.encode())
+    f.flush()
+    f.seek(0)
+    return f.readlines()
 
 
-def openurl(argument='',url=r'http://www.sodu.cc/result.html?searchstr=',code='utf-8'):
-    '''
+# 根据小说名称获取目录URL地址，存到bookname中
+def GetBookDirectoryUrl_Save():
+    str=__GetBookName()
+    # stone = stoneobject()
+    for s in str:
+        # print(s.decode())
+        book_name=s.decode().replace('\n', '', -1).replace('\r','',-1)
+        url=OpenUrl('http://www.sodu.cc/result.html?searchstr=' + book_name)
+        soup=ParsingString(url)
+        # print(soup.find_all(attrs='main-html'))
+        hrefs=soup.find_all(attrs='main-html')
+        for href in hrefs:
+            # print(href.a.get('href'))
+            if href.a.get('href') !=None:
+                BookDirectoryUrl=href.a.get('href')
+        try:
+            stone.query(BookName).filter(BookName.NovelName == book_name).one()
+        except NoResultFound:
+            stone.add(BookName(NovelName=book_name, BookDirectoryUrl=BookDirectoryUrl))
+            stone.commit()
+    # stone.close()
 
-    :param argument:查询参数
-    :param url:打开的地址
-    :param code:解码方式
-    :return:字符串string
-    '''
-    url=url+urllib.request.quote(argument)
-    # html=urllib.request.urlopen(url)
-    # 模拟浏览器
-    req=urllib.request.Request(url)
-    req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36')
-    return urllib.request.urlopen(req).read().decode(code)
+# 打开目录连接跳转到真实链接,并测试是否能打开，返回第一能打开的链接
+def OpenBookDirectoryUrl_Test_return(BookDirectoryUrl):
+    www=re.compile(r"(?:http|ftp)s?://.*?\.html")
+    html=OpenUrl(url=BookDirectoryUrl)
+    # print(html)
+    soup=ParsingString(html)
+    hrefs=soup.find_all(attrs='main-html')
+    for i,href in enumerate(hrefs):
+        # print(href.a.get('href'))
+        url=href.a.get('href')
+        bookchapter = href.a.string
+        # print('_____')
+        # print(bookchapter)
+        # print('____')
+        # print(url)
+        html=OpenUrl(url=url)
+        string=re.findall(www,html)
+        # print(string)
+        try:
+            # print(OpenUrl(url=string[0],encoding='gbk'))
+            OpenUrl(url=string[0])
+            # print(url)
+            return bookchapter,string[0]
+        except ConnectTimeout:
+            print('跳过第%s条' %(i+1))
+        if i+1>=10:
+            print('网络故障')
+            return None
 
-def bookselect(soup,select='.main-html'):
-    '''
+# 获取排名第一的（如果能打开,需要设置超时）网站，获取最新章节URL
+def GetChapterUrl_OK():
+    # stone = stoneobject()
+    # print('-------')
+    # print(stone.query(bookname).values(bookname.bookdirectoryurl))
+    NovelBooks=stone.query(BookName).order_by(BookName.id).values(BookName.NovelName, BookName.BookDirectoryUrl)
+    Novels = []
+    Novel_Names=[]
+    for Novel_Name,BookDirectoryUrl in NovelBooks:
+        # print(Novel_Name,BookDirectoryUrl)
+        # print(OpenBookDirectoryUrl_Test_return(BookDirectoryUrl))
+        Novel_Names.append(Novel_Name)
+        Novels.append(OpenBookDirectoryUrl_Test_return(BookDirectoryUrl))
+    for i,Novel in enumerate(Novels):
+        try:
+            stone.query(BookList).filter(BookList.bookchapter==Novel[0]).one()
+        except NoResultFound:
+            stone.add(BookList(bookname=Novel_Names[i],bookchapter=Novel[0],chapterurl=Novel[1],chaptercontent=None,chapterdate=datetime.now(),status=True))
+            stone.commit()
+    # return
 
-    :param soup: BeautifulSoup 对象
-    :return: 搜索页面中list对象 第一个是url，第二个是书名，第三个章节名，第四个是最近更新时间，
-              非搜索页面中，第一个是url，第二是章节名称，第三个是网站名称，第四个是更新时间，
-    '''
-    # 筛选class='.main-html'的所有类型（由于网页只可能是div）
-    filelist=soup.select(select)
-    # 获取链接、书名、章节名、时间
-    book=[]
-    for files in filelist:
-        # print(files.a.get('href'))
-        if (files.a!=None):
-            book.append(files.a.get('href'))
-        for file in files.stripped_strings:
-            if(file=='加入书架'):
-                continue
-            # print(file)
-            book.append(file)
-            # print(file.string)
-    return book
+# 根据自定义Mapping，选择文本所在标签的类名（class），将文本更新到数据表BookList中chaptercontent
+def SelectClass_SaveContent():
+    lists = stone.query(BookList).filter(BookList.status == True).all()
+    # name=
+    # print(string.readline())
+    for list in lists:
+        # print(type(list))
+        print(list.chapterurl)
+        html = OpenUrl(list.chapterurl)
+        soup = ParsingString(html)
+        for tag in mapping:
+            # print(soup.select(tag))
+            if soup.select(tag):
+                content = ''
+                for texts in soup.select(tag):
+                    # print( text.stripped_strings)
+                    for text in texts.stripped_strings:
+                        print(text)
+                        content = content + text + '\n'
+                stone.query(BookList).filter(BookList.id == list.id).update({BookList.chaptercontent: content})
+                stone.commit()
+# r=requests.get(url='http://www.168xs.com/du/121796/37676436.html')
+# print(r.content)
 
 def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
 def send(header,body):
-    from_addr = input("发件人邮箱")
-    password = input("发件人邮箱密码")
-    to_addr =input("收件人邮箱")
     smtp_server =r'smtp.qq.com'
     # smtp_server = r'smtp.163.com'
 
@@ -84,39 +185,31 @@ def send(header,body):
     server.sendmail(from_addr, [to_addr], msg.as_string())
     server.quit()
 
-if __name__=='__main__':
-    # print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    # sodu网页查询
-    str=input("小说名称")
+def Check_Send():
+    lists=stone.query(BookList).filter(BookList.status==True).all()
+    for list in lists:
+        if list.chaptercontent!=None:
+            send(list.bookchapter,list.chaptercontent)
+            stone.query(BookList).filter(BookList.id==list.id).update({BookList.status:False})
+            stone.commit()
+        else:
+            send(list.bookchapter+"无法解析", list.chapterurl+"请查看原链接，更新TAG")
+    try:
+        stone.query(BookList).filter(BookList.status==True,BookList.chaptercontent!=None).one()
+        Check_Send()
+    except NoResultFound:
+        time.sleep(60*10)
+# 检查表BookList中status为True的行，chaptercontent不为None则发送文本，否则发送错误，同时更新status
+# 发送完成后，检查是否还存在chaptercontent不为None且status为True的行，不存在休息15分钟，存在继续调用
+
+if __name__ == '__main__':
+# 获取目录URL
+    GetBookDirectoryUrl_Save()
     while True:
-        soup=bs(openurl(str),'html.parser')
-        # 获取网页查询第一个进行打开
-        chapterdate=bookselect(soup)[3]
-        bookname=bookselect(soup)[1]
-        bookchapter=bookselect(soup)[2]
-        # if timeend>timer :
-        #     timer=timeend
-            # print(1)
-        soup=bs(openurl(url=bookselect(soup)[0]),'html.parser')
-        # 将当前页面第一个链接进行打开后，通过正则表达式，获取真是链接
-        www=re.compile(r"(?:http|ftp)s?://.*?html")
-        # 打开链接，解码方式为GBK(卧槽）
-        chapterurl=(re.findall(www, openurl(url=bookselect(soup)[0]))[0])
-        soup=bs(openurl(url=chapterurl,code='gbk'),'html.parser')
-        # 筛选id=content，将值读取到book
-        book=bookselect(soup,select='#content')
-        # 重置
-        body=''
-        for b in book:
-            # print(b)
-            body= body+b +'\n'
-        print(body)
-        # send(header=header,body=body)
-        print(chapterdate)
         try:
-            query(bookchapter)
-        except NoResultFound:
-            save(bookname=str, bookchapter=bookchapter, chapterurl=chapterurl, chaptercontent=body,)
-            # print('更新')
-            send(header=str+" 更新 "+bookchapter,body=body)
-        time.sleep(15*60)
+            GetChapterUrl_OK()
+            SelectClass_SaveContent()
+            Check_Send()
+        except ConnectTimeout:
+            print("网络超时，1分钟后重试")
+            time.sleep(60)
--- stone.py
+++ stone.py
@@ -1,19 +1,22 @@
-# 导入:
-import sys
-
 import sqlite3
-from sqlalchemy import Column, String, create_engine, Integer,DateTime
-from sqlalchemy.orm import sessionmaker
+import sys
+from sqlalchemy import Column, Integer, String, DateTime, create_engine, Boolean
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+
+cx=sqlite3.connect(sys.path[0]+"/book.sqlite3")
+engine = create_engine("sqlite:///"+sys.path[0]+"/book.sqlite3", echo=True)
+# 创建DBSession类型:
+DBSession = sessionmaker(bind=engine)
 
 # 创建对象的基类:
 Base = declarative_base()
-
-# 定义User对象:
-class booklist(Base):
+# 连接数据库
+session = DBSession()
+# 定义booklist对象:
+class BookList(Base):
     # 表的名字:
-    __tablename__ = 'booklist'
+    __tablename__ = 'BookList'
 
     # 表的结构:
     id = Column(Integer(),primary_key=True)
@@ -22,33 +25,27 @@ class booklist(Base):
     chapterurl=Column(String(100))
     chaptercontent=Column(String(8000))
     chapterdate=Column(DateTime())
+    status=Column(Boolean())
     #
     # def __str__(self):
     #     return self.bookchapter
 
-# 初始化数据库连接:
-# print(sys.path[0])
-cx=sqlite3.connect(sys.path[0]+"/study.sqlite3")
-engine = create_engine("sqlite:///"+sys.path[0]+"/study.sqlite3", echo=True)
-# 创建DBSession类型:
-DBSession = sessionmaker(bind=engine)
+class BookName(Base):
+    # 表的名字:
+    __tablename__ = 'NovelName'
 
+    # 表的结构:
+    id = Column(Integer(),primary_key=True)
+    NovelName = Column(String(20))
+    BookDirectoryUrl=Column(String(100))
+    #
+    # def __str__(self):
+    #     return self.bookchapter
+
+# 如果没有创建表，则创建
 Base.metadata.create_all(engine)
-session = DBSession()
-
-def save(bookname,bookchapter,chapterurl,chaptercontent,chapterdate= datetime.now()):
-    session.add(booklist(bookname=bookname,bookchapter=bookchapter,chapterurl=chapterurl,chaptercontent=chaptercontent,chapterdate=chapterdate))
-    session.commit()
-    session.close()
-
-def query(str):
-    return session.query(booklist).filter(booklist.bookchapter==str).one()
 
 
-# for list in session.query(booklist).all():
-#     print(list.bookchapter)
-# print('______________')
-# print(session.query(booklist).filter(booklist.bookchapter=='第1214章 聊够了没').one().bookchapter)
-# def query():
-#     return session.query(booklist).one().bookchapter
-# NoResultFound
+def stoneobject():
+    return session
+
